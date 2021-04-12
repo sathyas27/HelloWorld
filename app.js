c465aea4949f48c58c7af059053c2f1c545b41be
Nagarajan Chinnasamy <nagarajanchinnasamy@gmail.com>
2021-04-12 07:11:07 +0530

    reqRespHandler introduced

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const reqRespHandler =(req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
-});
+};
+
+
+const server = http.createServer(reqRespHandler);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
